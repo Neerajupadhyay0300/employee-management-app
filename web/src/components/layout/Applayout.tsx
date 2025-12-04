68d6df1414e6d41225e2274ce1f8c1fddbb00fb4
Neerajupadhyay0300 <neerajupadhyay0300@gmail.com>
2025-12-04 12:51:58 +0530

    chore:remove unrealized imports

--- web/src/components/layout/Applayout.tsx
+++ web/src/components/layout/Applayout.tsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Sidebar from './Sidebar'
-import MainContent from './MainContant'
 
 interface AppLayoutProps {
   children?: React.ReactNode
